59d9da2e32e35c52c92bbf936100a7bb5cc83441
MahdiAw <mahdiawada127001@gmail.com>
2022-01-17 22:06:55 +0200

    Product gorup4

--- src/models/CPU.java
+++ src/models/CPU.java
@@ -1,5 +1,28 @@
 package models;
 
 public class CPU extends Product {
-	private 
+	private int CoresNb;
+	private int Freq;
+	private int Speed;
+	public int getCoresNb() {
+		return CoresNb;
+	}
+	public void setCoresNb(int coresNb) {
+		CoresNb = coresNb;
+	}
+	public int getFreq() {
+		return Freq;
+	}
+	public void setFreq(int freq) {
+		Freq = freq;
+	}
+	public int getSpeed() {
+		return Speed;
+	}
+	public void setSpeed(int speed) {
+		Speed = speed;
+	}
+	
+	
+	
 }
