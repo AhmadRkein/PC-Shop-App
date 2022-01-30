b958c5f742cc67ff187b576d8f6868ab73b11fb1
Ahmad Rkein <Ahmadrkein12@gmail.com>
2022-01-30 18:26:34 +0200

    Create create-table.sql

---
+++ SQL/create-table.sql
@@ -0,0 +1,87 @@
+create database PcShop
+use pcshop
+create table cpu(
+    id int not null,   
+    name varchar(30) not null,
+    brand char(10) null,
+    price FLOAT null,
+    image varchar(30) null,
+    coresNb int not null,
+    freq FLOAT not null,
+    PRIMARY KEY (id)
+)
+create table gpu(
+    id int not null,   
+    name varchar(30) not null,
+    brand char(10) null,
+    price FLOAT null,
+    image varchar(30) null,
+    clockSpeed FLOAT not null,
+    vram float not null,
+    busType VARCHAR(10) not null,
+    PRIMARY KEY (id)
+ 
+)
+create table ram(
+    id int not null,   
+    name varchar(30) not null,
+    brand char(10) null,
+    price FLOAT null,
+    image varchar(30) null,
+    size int not null,
+    dram int not null,
+    PRIMARY KEY (id)
+)
+create table monitor(
+    id int not null,   
+    name varchar(30) not null,
+    brand char(10) null,
+    price FLOAT null,
+    image varchar(30) null,
+    size int not null,
+    resolution VARCHAR(20) not null,
+    PRIMARY KEY (id) 
+ 
+)
+create table hardDisk(
+    id int not null,   
+    name varchar(30) not null,
+    brand char(10) null,
+    price FLOAT null,
+    image varchar(30) null,
+    storage int null,
+    PRIMARY KEY (id)
+)
+create table pc(
+    id int not null,   
+    name varchar(30) not null,
+    brand char(10) null,
+    price FLOAT null,
+    image varchar(30) null,
+    cpuId int null,
+    gpuId int null,
+    ramId int null,
+    monitorId int null,
+    hardDiskId int null,
+    PRIMARY KEY (id),
+    foreign key (cpuId) references cpu (id),
+    foreign key (gpuId) references gpu (id),
+    foreign key (ramId) references ram (id),
+    foreign key (monitorId) references monitor (id),
+    foreign key (hardDiskId) references hardDisk (id),
+)
+
+create table client(
+    userName VARCHAR(30) not null,
+    password VARCHAR(30) not null,
+    firstName VARCHAR(30) null,
+    lastName varchar(30) null,
+    primary key (userName)
+) 
+create table employee(
+    username VARCHAR(30) not null,
+    password VARCHAR(30) not null,
+    firstName VARCHAR(30) null,
+    lastName varchar(30) null,
+    primary key (userName)
+) 
