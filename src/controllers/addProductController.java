305826ae0b63e87a4321cfce47634d87d938fec4
nasser <moudy881@gmail.com>
2022-02-23 16:09:19 +0200

    implemented add products function+error checking

--- src/controllers/addProductController.java
+++ src/controllers/addProductController.java
@@ -80,6 +80,9 @@ public class addProductController {
         radioGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
             @Override
             public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1) {
+                txt_extra1.setText("");
+                txt_extra2.setText("");
+                txt_extra3.setText("");
                 label_extra1.setVisible(true);
                 label_extra2.setVisible(true);
                 label_extra3.setVisible(true);
@@ -132,47 +135,88 @@ public class addProductController {
 
     @FXML
     void reset_Handler(ActionEvent event) {
-
+        txt_name.setText("");
+        txt_brand.setText("");
+        txt_price.setText("");
+        txt_extra1.setText("");
+        txt_extra2.setText("");
+        txt_extra3.setText("");
     }
 
     @FXML
     void submit_handler(ActionEvent event) {
         DBModel dbModel= DBModel.getInstance();
         ProductFactory f=new ProductFactory();
-        Product product= f.GenerateProduct(type);
-        product.setName(txt_name.getText());
-        product.setBrand(txt_brand.getText());
-        product.setPrice(Float.parseFloat(txt_price.getText()));
-        if(type.equals("cpu")){
-            CPU cpu=(CPU)product;
-            cpu.setCoresNb(Integer.parseInt(txt_extra1.getText()));
-            cpu.setFreq(Float.parseFloat(txt_extra2.getText()));
-            dbModel.addCpu(cpu);
-        }
-        else if(type.equals("gpu")){
-            GPU gpu=(GPU)product;
-            gpu.setClockSpeed(Integer.parseInt(txt_extra1.getText()));
-            gpu.setVramSize(Integer.parseInt(txt_extra1.getText()));
-            gpu.setBusType(txt_extra3.getText());
-            dbModel.addGpu(gpu);
-        }
-        else if(type.equals("ram")){
-            Ram ram=(Ram)product;
-            ram.setSize(Integer.parseInt(txt_extra1.getText()));
-            dbModel.addRam(ram);
-        }
-        else if(type.equals("harddisk")){
-            HardDisk hdd=(HardDisk) product;
-            hdd.setStorage(Integer.parseInt(txt_extra1.getText()));
-            dbModel.addHardDisk(hdd);
-        }
-        else if(type.equals("monitor")){
-            Monitor monitor=(Monitor) product;
-            monitor.setSize(Integer.parseInt(txt_extra1.getText()));
-            monitor.setResolution(txt_extra2.getText());
-            dbModel.addMonitor(monitor);
-        }
+        boolean valid=true;
+        txt_name.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:black");
+        txt_brand.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:black");
+        txt_price.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:black");
+        txt_extra1.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:black");
+        txt_extra2.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:black");
+        txt_extra3.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:black");
 
+        if(txt_name.getText().equals("")){
+            txt_name.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:red;");
+            valid=false;
+        }
+        if(txt_brand.getText().equals("")){
+            txt_brand.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:red;");
+            valid=false;
+        }
+        if(txt_price.getText().equals("")||Float.parseFloat(txt_price.getText())<0){
+            txt_price.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:red;");
+            valid=false;
+        }
+        if(txt_extra1.getText().equals("")){
+            txt_extra1.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:red;");
+            valid=false;
+        }
+        if(txt_extra2.getText().equals("")&&(type.equals("gpu")||type.equals("monitor")||type.equals("cpu"))){
+            txt_extra2.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:red;");
+        }
+        if(txt_extra3.getText().equals("")&&type.equals("gpu")){
+            txt_extra3.setStyle("-fx-background-radius:8;-fx-border-radius:8;-fx-border-color:red;");
+        }
+        if(valid) {
+            Product product = f.GenerateProduct(type);
+            product.setName(txt_name.getText());
+            product.setBrand(txt_brand.getText());
+            product.setPrice(Float.parseFloat(txt_price.getText()));
+            if (type.equals("cpu")) {
+                CPU cpu = (CPU) product;
+                cpu.setCoresNb(Integer.parseInt(txt_extra1.getText()));
+                cpu.setFreq(Float.parseFloat(txt_extra2.getText()));
+                dbModel.addCpu(cpu);
+            } else if (type.equals("gpu")) {
+                if(!(txt_extra2.equals("")||txt_extra3.getText().equals(""))) {
+                    GPU gpu = (GPU) product;
+                    gpu.setClockSpeed(Integer.parseInt(txt_extra1.getText()));
+                    gpu.setVramSize(Integer.parseInt(txt_extra1.getText()));
+                    gpu.setBusType(txt_extra3.getText());
+                    dbModel.addGpu(gpu);
+                }
+            } else if (type.equals("ram")) {
+                Ram ram = (Ram) product;
+                ram.setSize(Integer.parseInt(txt_extra1.getText()));
+                dbModel.addRam(ram);
+            } else if (type.equals("harddisk")) {
+                HardDisk hdd = (HardDisk) product;
+                hdd.setStorage(Integer.parseInt(txt_extra1.getText()));
+                dbModel.addHardDisk(hdd);
+            } else if (type.equals("monitor")) {
+                if(!txt_extra2.equals("")){
+                Monitor monitor = (Monitor) product;
+                monitor.setSize(Integer.parseInt(txt_extra1.getText()));
+                monitor.setResolution(txt_extra2.getText());
+                dbModel.addMonitor(monitor);}
+            }
+            txt_name.setText("");
+            txt_brand.setText("");
+            txt_price.setText("");
+            txt_extra1.setText("");
+            txt_extra2.setText("");
+            txt_extra3.setText("");
+        }
 
 
     }
